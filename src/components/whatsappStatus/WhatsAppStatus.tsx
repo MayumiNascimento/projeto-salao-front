2e7e14242eb03395ecc2572e2e7e33f032fb044b
MayumiNascimento <mayuminascimento9@gmail.com>
2025-05-27 18:23:23 -0300

    atualizando tipo do transport socket

--- src/components/whatsappStatus/WhatsAppStatus.tsx
+++ src/components/whatsappStatus/WhatsAppStatus.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import io from 'socket.io-client';
 
 const socket = io(process.env.REACT_APP_SOCKET_URL, {
-  transports: ["websocket", "polling"],
+  transports: ["websocket"],
   withCredentials: true
 });
 
